0d25c317475b7f6dcd1edd56789578b91d2c46e3
Pratik Roy <roypratik63@gmail.com>
2023-10-02 00:00:21 +0530

    Changing pin 13 to D0

--- day_one/node_mcu_codes/blink_led.ino
+++ day_one/node_mcu_codes/blink_led.ino
@@ -1,10 +1,10 @@
 void setup() {                
-  pinMode(13, OUTPUT);       //setup pin 13 as an output
+  pinMode(D0, OUTPUT);       //setup pin 13 as an output
 }
 
 void loop() {
-  digitalWrite(13, HIGH);    // turn the LED on (HIGH outputs 5V)
+  digitalWrite(D0, HIGH);    // turn the LED on (HIGH outputs 5V)
   delay(500);                 // wait for 500 milliseconds
-  digitalWrite(13, LOW);     // turn the LED off (LOW outputs 0V)
+  digitalWrite(D0, LOW);     // turn the LED off (LOW outputs 0V)
   delay(500);                 // wait for 500 milliseconds
 }
